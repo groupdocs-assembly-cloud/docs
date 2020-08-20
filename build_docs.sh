2336fa24720937c6d0980ed27ea76e7fa2038d96
Dmitry Zotov <dmitry.zotov@aspose.com>
2020-08-20 10:39:08 +0300

    Linux script

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 git submodule update --init --recursive docs-cloud-common
 git submodule foreach git pull origin master
-rsync -avz content docs-cloud-common/content
+rm -rf ./docs-cloud-common/content/assembly/*
+cp -r ./content/* ./docs-cloud-common/content/
 cd ./docs-cloud-common
 hugo server -D
